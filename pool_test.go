f427b4718196396c04c375bf3a12ad412966d8f4
Jeevanandam M <jeeva@myjeeva.com>
2017-01-27 09:25:28 -0800

    added test case

---
+++ pool_test.go
@@ -0,0 +1,34 @@
+// Copyright (c) Jeevanandam M. (https://github.com/jeevatkm)
+// go-aah/pool source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package pool
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"aahframework.org/test/assert"
+)
+
+func TestBufferPool(t *testing.T) {
+	bufPool := NewPool(10, func() interface{} {
+		return &bytes.Buffer{}
+	})
+
+	// get from pool and put it back to pool
+	for idx := 0; idx < 5; idx++ {
+		buf := bufPool.Get().(*bytes.Buffer)
+		assert.NotNil(t, buf)
+
+		time.Sleep(5 * time.Millisecond)
+		bufPool.Put(buf)
+	}
+
+	// count
+	assert.Equal(t, 1, bufPool.Count())
+
+	// clean up
+	bufPool.Drain()
+}
